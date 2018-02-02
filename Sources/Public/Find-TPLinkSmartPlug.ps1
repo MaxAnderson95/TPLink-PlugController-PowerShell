6b5ee561c8900cff988787b7dba3266e3169a70e
Max Anderson <cctpmax@gmail.com>
2018-02-02 18:40:03 -0500

    Added some verbose logging and comments.

--- Sources/Public/Find-TPLinkSmartPlug.ps1
+++ Sources/Public/Find-TPLinkSmartPlug.ps1
@@ -9,15 +9,18 @@ Function Find-TPLinkSmartPlug {
         
     )
 
+    #Check for the IPv4 Resource files
+    Write-Verbose "Checking for the IPv4 Network Scanner resource files"
     $ResourceCheck = Test-Path "$PSScriptRoot\..\Resources\IPv4-Network-Scanner\"
 
     If (!$ResourceCheck) {
 
+        #If they're not found, error and end the script
         Write-Error "IPv4-Network-Scanner resource not found. Please re-clone/download the TPLink PowerShell repo."
         Break
         
     }
 
-    Write-Verbose "IPv4 Network Scanner resource found"
+    Write-Verbose "IPv4 Network Scanner resource files found"
 
 }
