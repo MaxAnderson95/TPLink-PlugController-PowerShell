547a0eb51362609f637ff66f1152cdd27ca719d7
Max Anderson <cctpmax@gmail.com>
2018-02-02 18:57:02 -0500

    Added discovery of IP and subnet info from NIC. Added network scan and filter.

--- Sources/Public/Find-TPLinkSmartPlug.ps1
+++ Sources/Public/Find-TPLinkSmartPlug.ps1
@@ -23,4 +23,25 @@ Function Find-TPLinkSmartPlug {
 
     Write-Verbose "IPv4 Network Scanner resource files found"
 
+    Try {
+        
+        #Get the IP address and CIDR of the current running machine
+        Write-Verbose "Attempting to get IP and subnet information from the machine"
+        $NetInfo = (Get-NetIPAddress -AddressFamily IPv4)[0] | Select-Object IPAddress,PrefixLength
+
+    }
+    
+    Catch {
+
+        Write-Error "Unable to find IP address or adapter information."
+        Break
+
+    }
+
+    #Start a network scan using 3rd party script
+    $ScanResults = ..\Resources\IPv4-Network-Scanner\IPv4NetworkScan.ps1 -IPv4Address $NetInfo.IPAddress -CIDR $NetInfo.PrefixLength -DisableDNSResolving -EnableMACResolving
+
+    #Filter the results for TPLink Smart Plugs
+    $FoundPlugs = $ScanResults | Where-Object { $_.Vendor -like "*TP-Link*" }
+
 }
