5cb3562a7a5d27132b94edecbd147297edf27d56
Max Anderson <cctpmax@gmail.com>
2018-02-02 18:36:48 -0500

    Added checking for the scanner resource files.

--- Sources/Public/Find-TPLinkSmartPlug.ps1
+++ Sources/Public/Find-TPLinkSmartPlug.ps1
@@ -4,8 +4,20 @@ Function Find-TPLinkSmartPlug {
 
     #>
 
+    [CmdletBinding()]
     Param (
         
     )
+
+    $ResourceCheck = Test-Path "$PSScriptRoot\..\Resources\IPv4-Network-Scanner\"
+
+    If (!$ResourceCheck) {
+
+        Write-Error "IPv4-Network-Scanner resource not found. Please re-clone/download the TPLink PowerShell repo."
+        Break
+        
+    }
+
+    Write-Verbose "IPv4 Network Scanner resource found"
 
 }
