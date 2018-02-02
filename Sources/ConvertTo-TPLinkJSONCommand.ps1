2c119fa9b8730c74e8f641bfe0cc677796e10359
Max Anderson <cctpmax@gmail.com>
2018-02-02 07:43:14 -0500

    Added notes to the comment based help, CSV import, error checking for missing csv file, and output of JSON.

--- Sources/ConvertTo-TPLinkJSONCommand.ps1
+++ Sources/ConvertTo-TPLinkJSONCommand.ps1
@@ -12,17 +12,39 @@ Function ConvertTo-TPLinkJSONCommand {
         PS > ConvertTo-TPLinkJSONCommand -Input "TurnOn"
         "{""system"":{""set_relay_state"":{""state"":1}}}"
 
+        .NOTES
+        This function is not designed to be exported or for public use, thus the function does not validate that the
+        input for "InputObject" is a friendly command that is accepted. It also will output an empty respose if one 
+        is not found. This function leaves these two things up to the functions calling them.
+
     #>
 
     Param (
 
         [Parameter(Mandatory=$True)]
-        [String]$Input
+        [String]$InputObject,
+
+        [Parameter()]
+        [String]$FilePath = 'FriendlyCommandMapping.csv'
+
 
     )
 
-    $FriendlyNameMapping = Import-Csv -Path 'FriendlyCommandMapping.csv'
+    Try {
+        
+        #Import the CSV containing the mappings between friendly names and their JSON formatted versions
+        $FriendlyNameMapping = Import-Csv -Path $FilePath
+    
+    }
+    Catch {
 
-    Write-Output $FriendlyNameMapping
+        Write-Error "CSV mapping file not found. Either the default mapping file is not present or the one specified in -FilePath was not found."
+        Break
 
-}
+    }
+
+    $JSON = ($FriendlyNameMapping | Where-Object { $_.Command -eq $InputObject }).JSON
+
+    Write-Output $JSON
+
+}   
