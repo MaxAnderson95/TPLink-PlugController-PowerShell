35e6cabdc1674a238e10b1cc148032bfcc56f04e
Max Anderson <cctpmax@gmail.com>
2018-02-02 07:14:49 -0500

    Added comment based help and the input param to ConvertTo-TPLinkJSONCommand.

--- Sources/ConvertFrom-TPLinkFriendlyCommand.ps1
+++ Sources/ConvertFrom-TPLinkFriendlyCommand.ps1
@@ -2,12 +2,25 @@ Function ConvertTo-TPLinkJSONCommand {
 
     <#
 
+        .SYNOPSIS
+        Takes a given "Friendly" input and outputs the corresponding JSON command that the TPLink plug will accept.
+
+        .PARAMETER Input
+        An approved "Friendly" command in string format
+
+        .EXAMPLE
+        PS > ConvertTo-TPLinkJSONCommand -Input "TurnOn"
+        "{""system"":{""set_relay_state"":{""state"":1}}}"
+
     #>
 
     Param (
 
+        [Parameter(Mandatory=$True)]
+        [String]$Input
+
     )
 
-
+    
 
 }
