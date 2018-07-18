dcba334ae3bf5ef0f39cf191b2b5b5b69fbbc915
Matthew Parker <mlparker@gfbinsurance.com>
2018-07-18 11:10:29 -0400

    Added Connect Logic to validate device

--- Sources/Public/Find-TPLinkSmartPlug.ps1
+++ Sources/Public/Find-TPLinkSmartPlug.ps1
@@ -47,7 +47,16 @@ Function Find-TPLinkSmartPlug {
     $ScanResults = . $PSScriptRoot\..\Resources\IPv4-Network-Scanner\IPv4NetworkScan.ps1 -IPv4Address $NetInfo.IPAddress -CIDR $NetInfo.PrefixLength -DisableDNSResolving -EnableMACResolving
 
     #Filter the results for TPLink Smart Plugs
-    $FoundPlugs = $ScanResults | Where-Object { $_.Vendor -like "*TP-Link*" }
+    $possible = $ScanResults | Where-Object { $_.Vendor -like "*TP-Link*" }
+
+    foreach ($ip in $possible) {
+        $connection = (New-Object Net.Sockets.TcpClient)
+        $connection.Connect($possible.IPv4Address,9999)
+        if ($connection.Connected) {
+            $FoundPlugs += $ip
+            }
+    }
+
 
     If ($FoundPlugs -eq $Null) {
 
